0932a122c0b28a93c24d748fedffc33843b31f85
learngitwithrahul <quotexjunction@gmail.com>
2025-02-01 16:59:48 +0000

    variables and dataType

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,8 +2,18 @@ const accountId = 12345
 let accountEmail = "rahul@google.com"
 var accountPassword = "1721"
 accountCity = "jaipur"
+let accountState;
 
 // accountId = 2 // not allowed
 
+accountEmail = "ra@ra.com"
+accountPassword = "2121"
+accountCity = "Delhi"
+
 console.log(accountId);
 
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
